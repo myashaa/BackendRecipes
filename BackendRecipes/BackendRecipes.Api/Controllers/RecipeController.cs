1d0cdf1e6086be620497de9be2e15fe80a3ef2c7
Masha <mianto017@mail.ru>
2021-06-14 23:23:08 +0300

    Вынесла конвертер в отдельный класс.

--- BackendRecipes/BackendRecipes.Api/Controllers/RecipeController.cs
+++ BackendRecipes/BackendRecipes.Api/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
 ﻿using BackendRecipes.Api.Dto;
+using BackendRecipes.Api.Сonverters;
 using BackendRecipes.Domain.Recipe;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -10,50 +11,18 @@ namespace BackendRecipes.Api.Controllers
     public class RecipeController : ControllerBase
     {
         private IRecipeService _recipeService;
-        public RecipeController( IRecipeService recipeService )
+        private IRecipeConverter _recipeConverter;
+        public RecipeController(IRecipeService recipeService, IRecipeConverter recipeConverter)
         {
             _recipeService = recipeService;
+            _recipeConverter = recipeConverter;
         }
 
         [HttpGet]
         [Route( "" )]
         public IActionResult GetAllRecipes()
         {
-            List<RecipeDto> recipes = _recipeService.GetRecipes().ConvertAll(x => ConvertToRecipeDto(x));
-            RecipeDto ConvertToRecipeDto(Recipe recipe)
-            {
-                return new RecipeDto
-                {
-                    Id = recipe.Id,
-                    ImageUrl = recipe.ImageUrl,
-                    Author = recipe.Author,
-                    Tags = recipe.Tags,
-                    Favorites = recipe.Favorites,
-                    Likes = recipe.Likes,
-                    Name = recipe.Name,
-                    Description = recipe.Description,
-                    CookingTimeInMinutes = recipe.CookingTimeInMinutes,
-                    TotalPersons = recipe.TotalPersons,
-                    Ingredients = recipe.Ingredients.ConvertAll(x => ConvertToIngredientDto(x)),
-                    Steps = recipe.Steps.ConvertAll(x => ConvertToStepDto(x))
-                };
-            }
-            IngredientDto ConvertToIngredientDto(Ingredient ingredient)
-            {
-                return new IngredientDto
-                {
-                    Title = ingredient.Title,
-                    Items = ingredient.Items
-                };
-            }
-            StepDto ConvertToStepDto(Step step)
-            {
-                return new StepDto
-                {
-                    Number = step.Number,
-                    Description = step.Description
-                };
-            }
+            List<RecipeDto> recipes = _recipeService.GetRecipes().ConvertAll(x => _recipeConverter.ConvertToRecipeDto(x));
             return Ok( recipes );
         }
     }
---
+++ BackendRecipes/BackendRecipes.Api/Сonverters/RecipeConverter.cs
@@ -0,0 +1,43 @@
+﻿using BackendRecipes.Api.Dto;
+using BackendRecipes.Domain.Recipe;
+
+namespace BackendRecipes.Api.Сonverters
+{
+    public class RecipeConverter: IRecipeConverter
+    {
+        public RecipeDto ConvertToRecipeDto(Recipe recipe)
+        {
+            return new RecipeDto
+            {
+                Id = recipe.Id,
+                ImageUrl = recipe.ImageUrl,
+                Author = recipe.Author,
+                Tags = recipe.Tags,
+                Favorites = recipe.Favorites,
+                Likes = recipe.Likes,
+                Name = recipe.Name,
+                Description = recipe.Description,
+                CookingTimeInMinutes = recipe.CookingTimeInMinutes,
+                TotalPersons = recipe.TotalPersons,
+                Ingredients = recipe.Ingredients.ConvertAll(x => ConvertToIngredientDto(x)),
+                Steps = recipe.Steps.ConvertAll(x => ConvertToStepDto(x))
+            };
+        }
+        private IngredientDto ConvertToIngredientDto(Ingredient ingredient)
+        {
+            return new IngredientDto
+            {
+                Title = ingredient.Title,
+                Items = ingredient.Items
+            };
+        }
+        private StepDto ConvertToStepDto(Step step)
+        {
+            return new StepDto
+            {
+                Number = step.Number,
+                Description = step.Description
+            };
+        }
+    }
+}
